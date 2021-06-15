4ce757c48afded8dcb099d45f19eb41457d4c6d4
Tara Z. Manicsic <tzmanics@gmail.com>
2021-06-15 00:27:02 -0400

    imports json list of products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import products from './products.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-stripe-checkout';
+  products: any = products;
 }
